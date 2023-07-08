f3b64ea5b35206f8c0f8fb10dd8e04071e4af554
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2023-07-08 16:46:16 -0300

    Fixed page responsivity

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-fit px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50']) }}>
     {{ $slot }}
 </button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,25 +20,19 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <div class="min-h-screen bg-gray-100 flex">
+        <div class="flex h-screen bg-gray-100 ">
             @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-
-            <div class="w-full fixed scroll-smooth h-screen overflow-y-auto">
-
-                @if (isset($header))
-                    <header class="bg-white shadow z-0">
-                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
-                        </div>
-                    </header>
-                @endif
                 <!-- Page Content -->
-                <main>
+                <main class=" w-full h-screen overflow-hidden overflow-y-auto ">
+                    @if (isset($header))
+                        <header class="bg-white shadow" >
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
+
+                        </header>
+                    @endif
                     {{ $slot }}
                 </main>
-            </div>
 
         </div>
 
