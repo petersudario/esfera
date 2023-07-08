f746210b88859dc3d9529e57e681072f1c0d91c2
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2023-07-08 15:21:23 -0300

    Returning created users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('user.list');
+        $users = User::all();
+
+        return view('user.list', [
+            'users' =>  $users
+        ]);
     }
 
     /**
